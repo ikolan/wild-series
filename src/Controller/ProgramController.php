abffb1ad412e2894aa940ac979d4ba369c93320f
Nicolas <ikolan@users.noreply.github.com>
2022-11-16 11:53:40 +0100

    Styling programs index page

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(name: 'app_program_')]
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/{id}', requirements: ['id' => '\d+'], methods: ["GET"], name: 'show')]
-    public function show(int $id): Response
+    #[Route('/', methods: ["GET"], name: 'index')]
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
+        return $this->render('program/index.html.twig', [
+            'programs' => $programs
+        ]);
+    }
+
+    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ["GET"], name: 'show')]
+    public function show(int $id, ProgramRepository $programRepository): Response
+    {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
         return $this->render('program/show.html.twig', [
-            'id' => $id
+            'program' => $program
         ]);
     }
 }
